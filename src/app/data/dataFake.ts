2490341411781057159e08dc09411febb31bd603
Paulo Ricardo <notadead5@gmail.com>
2022-10-31 02:13:04 -0300

    Dinamicidade do Page content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel anuncia um novo filme do homem de ferro",
+        "photoCover": "https://live.staticflickr.com/7047/6927546810_21d3e5f669_b.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Nova Série anunciada no disney+",
+        "description": "nova serie da marvel",
+        "photoCover": "https://www.trustedreviews.com/wp-content/uploads/sites/54/2018/11/Disney-streaming-service.jpg"
+    }
+]

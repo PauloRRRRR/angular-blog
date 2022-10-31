94e4eae840f1b51fd10b61798e213e22ce195514
Paulo Ricardo <notadead5@gmail.com>
2022-10-31 18:43:05 -0300

    Basic final style

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,8 +7,38 @@ export const dataFake = [
     },
     {
         "id":"2",
+        "title": "EA Lands Three-Game Deal With Marvel",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "photoCover": "https://meups.com.br/wp-content/uploads/2022/10/Jogos-da-Marvel-900x503.jpg"
+    },
+    {
+        "id":"3",
         "title": "Nova Série anunciada no disney+",
         "description": "nova serie da marvel",
         "photoCover": "https://www.trustedreviews.com/wp-content/uploads/sites/54/2018/11/Disney-streaming-service.jpg"
+    },
+    {
+        "id":"4",
+        "title": "Marvel developing another new TV series",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "photoCover": "https://media.gq-magazine.co.uk/photos/62b2edea2176d0b26a09d519/master/pass/GettyImages-1394210939.jpg"
+    },
+    {
+        "id":"5",
+        "title": "November 2's New Marvel Comics",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "photoCover": "http://static.hq-now.com/hqs/hqs/uploads/picture/image/795144/00.jpg"
+    },
+    {
+        "id":"6",
+        "title": "Kevin Feige reveals about Namor",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "photoCover": "https://s.yimg.com/ny/api/res/1.2/p2tbupMw7L5PtpHh74zBXg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM0NQ--/https://s.yimg.com/os/creatr-uploaded-images/2022-10/65abb370-4366-11ed-8efd-dcd5d6888d5b"
+    },
+    {
+        "id":"7",
+        "title": "Vision Series in Works at Marvel Studios",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "photoCover": "https://hips.hearstapps.com/hmg-prod/images/where-did-white-vision-go-1614957426.png?crop=0.420xw:1.00xh;0.463xw,0&resize=640:*"
     }
 ]
